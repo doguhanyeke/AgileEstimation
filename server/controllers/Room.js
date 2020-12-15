605e6ae91fcedc20ab61bf23d19c05055aab2d5f
doguhanyeke <doguyeke@gmail.com>
2020-12-15 23:58:11 +0300

    eslint fixes

--- server/controllers/Room.js
+++ server/controllers/Room.js
@@ -1,10 +1,10 @@
-const {Room, Vote, User} = require('../model/models')
+const { Room } = require('../model/models')
 
 var express = require('express')
-var jwt = require('jsonwebtoken');
+var jwt = require('jsonwebtoken')
 
 
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid')
 
 var router = express.Router()
 
@@ -13,16 +13,16 @@ const appSecret = 'dogu<3utku'
 const rooms = {}
 
 router.get('/create', (req, res) => {
-    const roomID = uuidv4();
-    const userID = uuidv4();
+    const roomID = uuidv4()
+    const userID = uuidv4()
 
-    var token = jwt.sign({ userID: userID, roomID: roomID, role:"admin" }, appSecret);
+    var token = jwt.sign({ userID: userID, roomID: roomID, role:'admin' }, appSecret)
 
-    const newRoom = new Room(roomID,userID);
+    const newRoom = new Room(roomID,userID)
     rooms[roomID] = newRoom
 
 
-    res.cookie('poker',token, { maxAge: 9000000, httpOnly: false });
+    res.cookie('poker',token, { maxAge: 9000000, httpOnly: false })
     res.status(200).json({ msg: 'pong' })
 })
 
